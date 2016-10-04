c21360f690cd67030f4f77a1491a875f74ed8402
Kaiiler <Kaiiler@live.se>
2016-10-04 11:22:38 +0200

    upload new folder and private project progress

--- Privat/Testsida/js/Scripts.js
+++ Privat/Testsida/js/Scripts.js
@@ -1,42 +1,8 @@
-$(function(){
-  var width   = 200,
-      height  = 44 * 4 + 20,
-      speed   = 300,
-      button  = $('#menu-button'),
-      overlay = $('#overlay'),
-      menu    = $('#hamburger-menu');
-
-  button.on('click',function(e){
-    if(overlay.hasClass('open')) {
-      animate_menu('close');
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
     } else {
-      animate_menu('open');
+        x.className = "topnav";
     }
-  });
-
-  overlay.on('click', function(e){
-    if(overlay.hasClass('open')) {
-      animate_menu('close');
-    }
-  });
-
-  $('a[href="#"]').on('click', function(e){
-    e.preventDefault();
-  });
-
-  function animate_menu(menu_toggle) {
-    if(menu_toggle == 'open') {
-      overlay.addClass('open');
-      button.addClass('on');
-      overlay.animate({opacity: 1}, speed);
-      menu.animate({width: width, height: height}, speed);
-    }
-
-    if(menu_toggle == 'close') {
-      button.removeClass('on');
-      overlay.animate({opacity: 0}, speed);
-      overlay.removeClass('open');
-      menu.animate({width: "0", height: 0}, speed);
-    }
-  }
-});
+}
